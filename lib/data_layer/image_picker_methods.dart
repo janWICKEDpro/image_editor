43997ddec491abc41afeb78b41ed46f86d0caffe
janWICKEDpro <janroyal45@gmail.com>
2023-02-26 14:33:57 +0100

    image picker class in progress

--- lib/data_layer/image_picker_methods.dart
+++ lib/data_layer/image_picker_methods.dart
@@ -2,4 +2,8 @@ import 'package:image_picker/image_picker.dart';
 
 class ImagePickerMethods {
   final imagePickerClassIncstance = ImagePicker();
+
+  void pickFromGallery() {}
+
+  void pickFromCamera() {}
 }
