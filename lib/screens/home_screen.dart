227e8daf0c558362c27862027def477e3abbee8f
janWICKEDpro <janroyal45@gmail.com>
2023-02-28 18:42:17 +0100

    appbar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,7 +6,9 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        centerTitle: true,
+      ),
       body: Center(
           child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
