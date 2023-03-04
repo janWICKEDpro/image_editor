994c1cf825743c20390a5738947fce0528df0ffb
janWICKEDpro <janroyal45@gmail.com>
2023-03-04 19:05:42 +0100

    neomorphic container

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -21,24 +21,39 @@ class HomeScreen extends StatelessWidget {
               boxShadow: [
                 BoxShadow(
                   color: Colors.white.withOpacity(0.8),
-                  offset: Offset(-6.0, -6.0),
+                  offset: const Offset(-6.0, -6.0),
                   blurRadius: 16.0,
                 ),
                 BoxShadow(
                   color: Colors.black.withOpacity(0.1),
-                  offset: Offset(6.0, 6.0),
+                  offset: const Offset(6.0, 6.0),
                   blurRadius: 16.0,
                 ),
               ],
-              color: Color(0xFFEFEEEE),
+              color: const Color(0xFFEFEEEE),
               borderRadius: BorderRadius.circular(12.0),
             ),
           ),
           Container(
-            height: 100,
             width: 100,
-            color: Colors.purpleAccent,
-          )
+            height: 100,
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.white.withOpacity(0.8),
+                  offset: const Offset(-6.0, -6.0),
+                  blurRadius: 16.0,
+                ),
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.1),
+                  offset: const Offset(6.0, 6.0),
+                  blurRadius: 16.0,
+                ),
+              ],
+              color: const Color(0xFFEFEEEE),
+              borderRadius: BorderRadius.circular(12.0),
+            ),
+          ),
         ],
       )),
     );
