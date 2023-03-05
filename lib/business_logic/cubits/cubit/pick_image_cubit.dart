4666d58c5d984cdc778fcf6a54f730e30c7a7242
janWICKEDpro <janroyal45@gmail.com>
2023-03-05 12:37:36 +0100

    added some cubits

--- lib/business_logic/cubits/cubit/pick_image_cubit.dart
+++ lib/business_logic/cubits/cubit/pick_image_cubit.dart
@@ -1,13 +1,18 @@
 import 'package:bloc/bloc.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:image_processing/data_layer/image_picker_methods.dart';
 import 'package:meta/meta.dart';
 
 part 'pick_image_state.dart';
 
 class PickImageCubit extends Cubit<PickImageState> {
   PickImageCubit() : super(const PickImageState());
+  final pickImage = ImagePickerMethods();
 
-  void pickImageCamera() {}
+  void pickImageCamera() async {
+    final file = await pickImage.pickFromCamera();
+    emit(PickImageState(image: file));
+  }
 
   void pickImageGallery() {}
 }
