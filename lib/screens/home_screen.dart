804bf11d38652c358c24a18e17119df6e7eeeda4
janWICKEDpro <janroyal45@gmail.com>
2023-03-06 22:50:41 +0100

    few modifications

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,21 +9,62 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => PickImageCubit(),
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Color.fromARGB(255, 133, 28, 28),
-          title: const Text("Image Editor"),
-          centerTitle: true,
-        ),
-        body: Center(
-            child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            InkWell(
-              onTap: () {
-                BlocProvider.of<PickImageCubit>(context).pickImageGallery();
-              },
-              child: Column(
+      child: BlocListener<PickImageCubit, PickImageState>(
+        listener: (context, state) {
+          // TODO: implement listener
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Color.fromARGB(255, 133, 28, 28),
+            title: const Text("Image Editor"),
+            centerTitle: true,
+          ),
+          body: Center(
+              child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              InkWell(
+                onTap: () {
+                  BlocProvider.of<PickImageCubit>(context).pickImageGallery();
+                },
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 100,
+                        height: 100,
+                        decoration: BoxDecoration(
+                          image: const DecorationImage(
+                              image: AssetImage("assets/images/gallery3.ico")),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Color.fromARGB(255, 245, 243, 243)
+                                  .withOpacity(0.8),
+                              offset: Offset(-6.0, -6.0),
+                              blurRadius: 16.0,
+                            ),
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.1),
+                              offset: const Offset(6.0, 6.0),
+                              blurRadius: 16.0,
+                            ),
+                          ],
+                          color: Color.fromARGB(255, 250, 242, 242),
+                          borderRadius: BorderRadius.circular(12.0),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const Text("Gallery",
+                          style: TextStyle(fontFamily: 'RobotoMono'))
+                    ]),
+              ),
+              InkWell(
+                onTap: () {
+                  BlocProvider.of<PickImageCubit>(context).pickImageCamera();
+                },
+                child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Container(
@@ -31,12 +72,12 @@ class HomeScreen extends StatelessWidget {
                       height: 100,
                       decoration: BoxDecoration(
                         image: const DecorationImage(
-                            image: AssetImage("assets/images/gallery3.ico")),
+                            image: AssetImage("assets/images/camera.ico")),
                         boxShadow: [
                           BoxShadow(
                             color: Color.fromARGB(255, 245, 243, 243)
                                 .withOpacity(0.8),
-                            offset: Offset(-6.0, -6.0),
+                            offset: const Offset(-6.0, -6.0),
                             blurRadius: 16.0,
                           ),
                           BoxShadow(
@@ -52,49 +93,13 @@ class HomeScreen extends StatelessWidget {
                     const SizedBox(
                       height: 10,
                     ),
-                    const Text("Gallery",
-                        style: TextStyle(fontFamily: 'RobotoMono'))
-                  ]),
-            ),
-            InkWell(
-              onTap: () {
-                BlocProvider.of<PickImageCubit>(context).pickImageCamera();
-              },
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    width: 100,
-                    height: 100,
-                    decoration: BoxDecoration(
-                      image: const DecorationImage(
-                          image: AssetImage("assets/images/camera.ico")),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Color.fromARGB(255, 245, 243, 243)
-                              .withOpacity(0.8),
-                          offset: const Offset(-6.0, -6.0),
-                          blurRadius: 16.0,
-                        ),
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.1),
-                          offset: const Offset(6.0, 6.0),
-                          blurRadius: 16.0,
-                        ),
-                      ],
-                      color: Color.fromARGB(255, 250, 242, 242),
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Text("Camera", style: TextStyle(fontFamily: 'RobotoMono'))
-                ],
+                    Text("Camera", style: TextStyle(fontFamily: 'RobotoMono'))
+                  ],
+                ),
               ),
-            ),
-          ],
-        )),
+            ],
+          )),
+        ),
       ),
     );
   }
