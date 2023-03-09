422a5ba3f76abfa51e2afab65b7aff495736e785
janWICKEDpro <janroyal45@gmail.com>
2023-03-09 08:26:13 +0100

    added functionality to change views

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,103 +2,110 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_processing/business_logic/cubits/cubit/pick_image_cubit.dart';
 import 'package:image_processing/constants/constants.dart';
+import 'package:image_processing/screens/edit_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocListener<PickImageCubit, PickImageState>(
-      listener: (context, state) {},
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 133, 28, 28),
-          title: const Text("Image Editor"),
-          centerTitle: true,
-        ),
-        body: Center(
-            child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            InkWell(
-              splashColor: Colors.white,
-              onTap: () {
-                BlocProvider.of<PickImageCubit>(context).pickImageGallery();
-              },
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      width: 100,
-                      height: 100,
-                      decoration: BoxDecoration(
-                        image: const DecorationImage(
-                            image: AssetImage("assets/images/gallery3.ico")),
-                        boxShadow: [
-                          BoxShadow(
-                            color: const Color.fromARGB(255, 245, 243, 243)
-                                .withOpacity(0.8),
-                            offset: const Offset(-6.0, -6.0),
-                            blurRadius: 16.0,
+    return BlocBuilder<PickImageCubit, PickImageState>(
+      builder: (context, state) {
+        if (state.image == null) {
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: const Color.fromARGB(255, 133, 28, 28),
+              title: const Text("Image Editor"),
+              centerTitle: true,
+            ),
+            body: Center(
+                child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                InkWell(
+                  splashColor: Colors.white,
+                  onTap: () {
+                    BlocProvider.of<PickImageCubit>(context).pickImageGallery();
+                  },
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 100,
+                          height: 100,
+                          decoration: BoxDecoration(
+                            image: const DecorationImage(
+                                image:
+                                    AssetImage("assets/images/gallery3.ico")),
+                            boxShadow: [
+                              BoxShadow(
+                                color: const Color.fromARGB(255, 245, 243, 243)
+                                    .withOpacity(0.8),
+                                offset: const Offset(-6.0, -6.0),
+                                blurRadius: 16.0,
+                              ),
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.1),
+                                offset: const Offset(6.0, 6.0),
+                                blurRadius: 16.0,
+                              ),
+                            ],
+                            color: const Color.fromARGB(255, 250, 242, 242),
+                            borderRadius: BorderRadius.circular(12.0),
                           ),
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.1),
-                            offset: const Offset(6.0, 6.0),
-                            blurRadius: 16.0,
-                          ),
-                        ],
-                        color: const Color.fromARGB(255, 250, 242, 242),
-                        borderRadius: BorderRadius.circular(12.0),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        const Text("Gallery",
+                            style: TextStyle(fontFamily: 'RobotoMono'))
+                      ]),
+                ),
+                InkWell(
+                  splashColor: Colors.white,
+                  onTap: () {
+                    BlocProvider.of<PickImageCubit>(context).pickImageCamera();
+                  },
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 100,
+                        height: 100,
+                        decoration: BoxDecoration(
+                          image: const DecorationImage(
+                              image: AssetImage("assets/images/camera.ico")),
+                          boxShadow: [
+                            BoxShadow(
+                              color: const Color.fromARGB(255, 245, 243, 243)
+                                  .withOpacity(0.8),
+                              offset: const Offset(-6.0, -6.0),
+                              blurRadius: 16.0,
+                            ),
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.1),
+                              offset: const Offset(6.0, 6.0),
+                              blurRadius: 16.0,
+                            ),
+                          ],
+                          color: const Color.fromARGB(255, 250, 242, 242),
+                          borderRadius: BorderRadius.circular(12.0),
+                        ),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    const Text("Gallery",
-                        style: TextStyle(fontFamily: 'RobotoMono'))
-                  ]),
-            ),
-            InkWell(
-              splashColor: Colors.white,
-              onTap: () {
-                BlocProvider.of<PickImageCubit>(context).pickImageCamera();
-              },
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    width: 100,
-                    height: 100,
-                    decoration: BoxDecoration(
-                      image: const DecorationImage(
-                          image: AssetImage("assets/images/camera.ico")),
-                      boxShadow: [
-                        BoxShadow(
-                          color: const Color.fromARGB(255, 245, 243, 243)
-                              .withOpacity(0.8),
-                          offset: const Offset(-6.0, -6.0),
-                          blurRadius: 16.0,
-                        ),
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.1),
-                          offset: const Offset(6.0, 6.0),
-                          blurRadius: 16.0,
-                        ),
-                      ],
-                      color: const Color.fromARGB(255, 250, 242, 242),
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      text("Camera")
+                    ],
                   ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  text("Camera")
-                ],
-              ),
-            ),
-          ],
-        )),
-      ),
+                ),
+              ],
+            )),
+          );
+        } else {
+          return EditScreen();
+        }
+      },
     );
   }
 }
