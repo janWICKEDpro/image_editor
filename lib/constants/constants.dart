553d54f5bdc89d0b0c09767eb676a4d718a01ae7
janWICKEDpro <janroyal45@gmail.com>
2023-03-15 19:33:41 +0100

    added cancel selected image function

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
-Widget text(String text) {
+Widget text(String text, [Color? color]) {
   return Text(
     text,
-    style: const TextStyle(
+    style: TextStyle(
+      fontSize: 15,
+      color: color,
       fontFamily: 'RobotoMono',
     ),
   );
--- lib/screens/edit_screen.dart
+++ lib/screens/edit_screen.dart
@@ -17,52 +17,61 @@ class _EditScreenState extends State<EditScreen> {
   Widget build(BuildContext context) {
     final image = BlocProvider.of<PickImageCubit>(context).state.image;
 
-    return Container(
-      height: MediaQuery.of(context).size.height,
-      width: MediaQuery.of(context).size.width,
-      decoration: const BoxDecoration(
-          backgroundBlendMode: BlendMode.darken,
-          color: Color.fromARGB(255, 10, 8, 8)),
-      child: Column(
-        children: [
-          const SizedBox(
-            height: 20,
-          ),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.cancel_outlined,
-                    color: Colors.white,
-                  )),
-              IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.save_alt_outlined,
-                    color: Colors.white,
-                  )),
-            ],
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Row(
-            children: [
-              TextButton(onPressed: () {}, child: text("Rotate")),
-              TextButton(onPressed: () {}, child: text("crop"))
-            ],
-          ),
-          // crop / rotate selectables
-          Container(
-            height: 30,
-            color: Colors.red,
-          ),
+    return Material(
+      child: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: const BoxDecoration(
+            backgroundBlendMode: BlendMode.darken,
+            color: Color.fromARGB(255, 10, 8, 8)),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                    onPressed: () {
+                      BlocProvider.of<PickImageCubit>(context)
+                          .canclePickedImage();
+                    },
+                    icon: const Icon(
+                      Icons.cancel_outlined,
+                      color: Colors.white,
+                    )),
+                IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.save_alt_outlined,
+                      color: Colors.white,
+                    )),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                    onPressed: () {}, child: text("Rotate", Colors.white)),
+                text("|", Colors.white),
+                TextButton(onPressed: () {}, child: text("crop", Colors.white))
+              ],
+            ),
+            // crop / rotate selectables
+            Container(
+              height: 30,
+              color: Colors.red,
+            ),
 
-          Image.file(File(image!.path))
-        ],
+            Image.file(File(image!.path))
+          ],
+        ),
       ),
     );
   }
