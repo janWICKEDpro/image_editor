a96aa0484201d876c9e99019e17cb15612e2673e
janWICKEDpro <janroyal45@gmail.com>
2023-03-17 12:24:45 +0100

    added method to crop image

--- lib/business_logic/crop_image/crop_image_cubit.dart
+++ lib/business_logic/crop_image/crop_image_cubit.dart
@@ -1,8 +1,36 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:image_cropper/image_cropper.dart';
 import 'package:image_processing/business_logic/crop_image/crop_image_state.dart';
 
 class CropImageCubit extends Cubit<CropImageState> {
   CropImageCubit() : super(CropImageState());
 
-  void cropImage() async {}
+  void cropImage(String sourcePath, BuildContext context) async {
+    final croppedFile = await ImageCropper().cropImage(
+        sourcePath: sourcePath,
+        compressFormat: ImageCompressFormat.jpg,
+        compressQuality: 100,
+        uiSettings: [
+          AndroidUiSettings(
+              toolbarTitle: 'Cropper',
+              toolbarColor: Colors.deepOrange,
+              toolbarWidgetColor: Colors.white,
+              initAspectRatio: CropAspectRatioPreset.original,
+              lockAspectRatio: false),
+          IOSUiSettings(
+            title: 'Cropper',
+          ),
+          WebUiSettings(
+            context: context,
+            presentStyle: CropperPresentStyle.dialog,
+            boundary: const CroppieBoundary(
+              width: 520,
+              height: 520,
+            ),
+          )
+        ]);
+
+    emit(CropImageState(croppedFile: croppedFile));
+  }
 }
