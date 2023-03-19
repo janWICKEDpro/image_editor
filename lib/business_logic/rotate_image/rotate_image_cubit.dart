844dc71abe7d04de9388d1bdf0c97636874a3394
janWICKEDpro <janroyal45@gmail.com>
2023-03-19 20:30:34 +0100

    added the rotate image state

---
+++ lib/business_logic/rotate_image/rotate_image_cubit.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:image_processing/business_logic/rotate_image/rotate_image_state.dart';
+
+class RotateImageCubit extends Cubit<RotateImageState> {
+  RotateImageCubit(super.initialState);
+}
