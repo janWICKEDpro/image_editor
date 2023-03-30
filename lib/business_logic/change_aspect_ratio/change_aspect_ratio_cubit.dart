6745e7e219ca3511b7d23827a305c8440cb238f4
janWICKEDpro <janroyal45@gmail.com>
2023-03-30 21:06:36 +0100

    change aspect ratio

--- lib/business_logic/change_aspect_ratio/change_aspect_ratio_cubit.dart
+++ lib/business_logic/change_aspect_ratio/change_aspect_ratio_cubit.dart
@@ -4,6 +4,7 @@ import 'package:image_processing/business_logic/change_aspect_ratio/change_aspec
 
 class ChangeAspectRatioCubit extends Cubit<ChangeAspectRatioState> {
   ChangeAspectRatioCubit() : super(ChangeAspectRatioState());
+  //resize the picture to  a different aspect ratio
   void changeAspectRatio(double ratio) {
     emit(ChangeAspectRatioState(aspectRatio: ratio));
   }
