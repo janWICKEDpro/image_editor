3ae9c77d6e4d010e20fad74d21a7395d5fe790b6
janWICKEDpro <janroyal45@gmail.com>
2023-04-07 20:02:42 +0100

    code refactor

--- lib/business_logic/change_aspect_ratio/change_aspect_ratio_cubit.dart
+++ lib/business_logic/change_aspect_ratio/change_aspect_ratio_cubit.dart
@@ -3,7 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_processing/business_logic/change_aspect_ratio/change_aspect_ratio_state.dart';
 
 class ChangeAspectRatioCubit extends Cubit<ChangeAspectRatioState> {
-  ChangeAspectRatioCubit() : super(ChangeAspectRatioState());
+  ChangeAspectRatioCubit() : super(ChangeAspectRatioState(aspectRatio: 1.0));
   //resize the picture to  a different aspect ratio
   void changeAspectRatio(double aspectRatio) {
     emit(ChangeAspectRatioState(aspectRatio: aspectRatio));
