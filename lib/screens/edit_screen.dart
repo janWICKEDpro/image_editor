d08a0310739a158ec3d1cc3ff40837f8750ba8b0
janWICKEDpro <janroyal45@gmail.com>
2023-04-30 20:16:14 +0100

    cleanup

--- lib/screens/edit_screen.dart
+++ lib/screens/edit_screen.dart
@@ -21,7 +21,6 @@ class _EditScreenState extends State<EditScreen> {
   Widget build(BuildContext context) {
     final image = BlocProvider.of<PickImageCubit>(context).state.image;
 
-    // todo: rework on the ui
     return Material(
       child: Container(
         height: MediaQuery.of(context).size.height,
