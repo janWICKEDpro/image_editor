880bf9acace621cd7f5c0d88e5475f9f6045ceb9
janWICKEDpro <janroyal45@gmail.com>
2023-04-30 20:21:02 +0100

    edited the text() widget

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 Widget text(String text, [Color? color]) {
   return Text(
     text,
+    softWrap: true,
     style: TextStyle(
       fontSize: 15,
       color: color,
