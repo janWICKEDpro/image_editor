f54cc47961c8d1133d066c99a350cea8104854d5
janWICKEDpro <janroyal45@gmail.com>
2023-04-30 20:20:16 +0100

    removed some repeatitive if statement

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -13,18 +13,14 @@ Widget text(String text, [Color? color]) {
 
 //set the appropriate aspect ratio
 double getAppropriateAspectRatio(int index) {
-  if (index != 1) {
-    switch (index) {
-      case 0:
-        return 1.0;
-      case 2:
-        return 0.5;
-      case 3:
-        return 2 / 3;
-      default:
-        return 1.0;
-    }
-  } else {
-    return 1.0;
+  switch (index) {
+    case 0:
+      return 1.0;
+    case 2:
+      return 0.5;
+    case 3:
+      return 2 / 3;
+    default:
+      return 1.0;
   }
 }
