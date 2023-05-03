bf42b8f67f3bfa4ecb09b94dbfed1adbccdf900e
janWICKEDpro <janroyal45@gmail.com>
2023-05-03 08:43:35 +0100

    made some fields final

--- lib/business_logic/crop_image/crop_image_state.dart
+++ lib/business_logic/crop_image/crop_image_state.dart
@@ -1,6 +1,6 @@
 import 'package:image_cropper/image_cropper.dart';
 
 class CropImageState {
-  CropImageState({this.croppedFile});
-  CroppedFile? croppedFile;
+  const CropImageState({this.croppedFile});
+  final CroppedFile? croppedFile;
 }
