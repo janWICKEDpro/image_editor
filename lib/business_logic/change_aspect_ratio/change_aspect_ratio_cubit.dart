67386a3c69df853c3f465c5c83ff1fa3e58a0e48
janWICKEDpro <janroyal45@gmail.com>
2023-05-15 02:11:08 +0100

    updated to flutter 3.10

--- lib/business_logic/change_aspect_ratio/change_aspect_ratio_cubit.dart
+++ lib/business_logic/change_aspect_ratio/change_aspect_ratio_cubit.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_processing/business_logic/change_aspect_ratio/change_aspect_ratio_state.dart';
 
